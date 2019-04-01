5f631bd33df2d18714af164d4d7decdb9e062ac9
liuleilei1 <1359706333@qq.com>
2019-04-01 09:20:09 +0800

    遍历文件夹

---
+++ src/com/soft1841/util/FolderTest.java
@@ -0,0 +1,22 @@
+package com.soft1841.util;
+
+import java.io.File;
+
+public class FolderTest {
+
+        public static void main(String[] args) {
+            //要遍历的路径
+            String path = "D:\\360downloads";
+            //获取其file对象
+            File file = new File(path);
+            //遍历path下的文件和目录，放在File数组中
+            File[] fs = file.listFiles();
+            //遍历File[]数组
+            for(File f:fs){
+                //若非目录(即文件)，则打印
+                if(!f.isDirectory())
+                    System.out.println(f);
+            }
+        }
+    }
+
